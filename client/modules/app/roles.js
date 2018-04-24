e08d13203bfc8f325d2dfa64bb7ee49cd39c12ea
Stéphane Bourbeau <stephane.bourbeau@telus.com>
2018-04-24 11:38:53 -0400

    correct role id

--- client/modules/app/roles.js
+++ client/modules/app/roles.js
@@ -1,4 +1,4 @@
 export const FORM_MANAGER = 'FORMS'
-export const ACCOUNT_MANAGER = 'USER'
-export const INTERACT_WITH_CLIENT = 'CLIENT'
+export const ACCOUNT_MANAGER = 'USERS'
+export const INTERACT_WITH_CLIENT = 'CLIENTS'
 export const STATS_PRODUCER = 'STATS'
